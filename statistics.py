bbf596fedab957d3c8ca86fc03e7d8285e612264
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2021-10-21 15:16:11 +0530

    Update statistics.py
    Updated computation of avg, max and min

--- statistics.py
+++ statistics.py
@@ -1,3 +1,13 @@
 
 def calculateStats(numbers):
-  return None
+  if(len(numbers)):
+    avg=sum(numbers)/len(numbers)
+    numbers.sort()
+    max = numbers[-1]
+    min = numbers[0]
+  else:
+    avg=0
+    max=0
+    min=0
+  computedStats = {'avg' :avg, 'max' : max, 'min' : min}
+  return computedStats
