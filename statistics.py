47bf8c731aa00ff73fa05735603185411b846547
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2021-10-21 15:49:50 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -22,17 +22,15 @@ class StatsAlerter:
         if computedStats["max"] > self.maxThreshold:
             self.alerts[0].emailSent = True
             self.alerts[1].ledGlows = True
-      
-      
+
+
 def calculateStats(numbers):
-  if(len(numbers)):
-    avg=sum(numbers)/len(numbers)
-    numbers.sort()
-    max = numbers[-1]
-    min = numbers[0]
-  else:
-    avg=0
-    max=0
-    min=0
-  computedStats = {'avg' :avg, 'max' : max, 'min' : min}
-  return computedStats
+    if len(numbers):
+        average = sum(numbers) / len(numbers)
+        maximum = max(numbers)
+        minimum = min(numbers)
+        computedStats = {"avg": average, "max": maximum, "min": minimum}
+    else:
+        computedStats = {"avg": math.nan, "max": math.nan, "min": math.nan}
+
+    return computedStats
